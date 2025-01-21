cb756656e65e76eb9a21083e4bc57f9cc4c92a22
KarthikeyanBT <btkkeyan@gmail.com>
2025-01-21 21:12:51 +0530

    21jan2025-updated

---
+++ src/ex_02_java_Basic/lab0017_variable.java
@@ -0,0 +1,4 @@
+package ex_02_java_Basic;
+
+public class lab0017_variable {
+}
--- src/ex_06_Ternary_Operator/Challange.java
+++ src/ex_06_Ternary_Operator/Challange.java
@@ -2,8 +2,8 @@ package ex_06_Ternary_Operator;
 
 public class Challange {
     public static void main(String[] args) {
-       int marks  = 50;
-        String grade = ((marks >=90)? "Grade A":((marks>=80)? "Grade B": (marks>=70)? "Grade C": (marks>=60)? "Grade D": "Grade E"));
+       int marks  = 66;
+        String grade = ((marks >=90)? "Grade A":((marks>=80)? "Grade B": (marks>=70)? "Grade C": (marks>=60)? "Grade D": "F"));
         System.out.println(grade);
     }
 }

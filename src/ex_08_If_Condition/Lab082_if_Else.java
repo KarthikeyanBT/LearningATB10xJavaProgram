5ac325d69e5d0a50446bb8465c3dad7953a1df79
KarthikeyanBT <btkkeyan@gmail.com>
2025-01-26 22:11:27 +0530

    26jan2025

---
+++ src/ex_08_If_Condition/Lab082_if_Else.java
@@ -0,0 +1,21 @@
+package ex_08_If_Condition;
+
+import java.util.Scanner;
+
+public class Lab082_if_Else {
+    public static void main(String[] args) {
+        // Allowed to vote or not
+        // If age > 18 -> allowed to vote.
+        // else age < >18 -> Not allowed to vote.
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age!(as integer)");
+        int age = sc.nextInt(); // java.util.InputMismatchException
+        if(age>18){
+            System.out.println("Your are eligible for vote");
+        } else
+        {
+            System.out.println("Your are not eligible for vote");
+        }
+    }
+}
